907773c0928e6a1af837192829290f6da723ec4f
Bruno Hidalgo <b.h.lemergas@gmail.com>
2023-06-08 22:29:17 +1200

    add: corner case tests & constraints in functions

--- lru.go
+++ lru.go
@@ -9,6 +9,9 @@ type LRUCache struct {
 }
 
 func Constructor(capacity int) LRUCache {
+	if capacity < 1 || capacity > 1000 {
+		panic("Capacity must be between 1 and 1000")
+	}
 	return LRUCache{
 		capacity: capacity,
 		cache:    make(map[int]int),
@@ -26,6 +29,12 @@ func remove(slice []int, s int) []int {
 }
 
 func (c *LRUCache) Put(key int, value int) {
+	if key < 0 || key > 103 {
+		panic("Key must be between 0 and 103")
+	}
+	if value < 0 || value > 105 {
+		panic("Value must be between 0 and 105")
+	}
 	if len(c.keys) >= c.capacity {
 		oldestKey := c.keys[0]
 		c.keys = c.keys[1:]
@@ -36,6 +45,9 @@ func (c *LRUCache) Put(key int, value int) {
 }
 
 func (c *LRUCache) Get(key int) int {
+	if key < 0 || key > 103 {
+		panic("Key must be between 0 and 103")
+	}
 	if val, ok := c.cache[key]; ok {
 		c.keys = remove(c.keys, key)
 		c.keys = append(c.keys, key)
@@ -45,7 +57,11 @@ func (c *LRUCache) Get(key int) int {
 }
 
 func (c *LRUCache) Delete(key int) int {
+	if key < 0 || key > 103 {
+		panic("Key must be between 0 and 103")
+	}
 	if val, ok := c.cache[key]; ok {
+		c.keys = remove(c.keys, key)
 		delete(c.cache, key)
 		return val
 	}
@@ -69,7 +85,9 @@ func main() {
 	fmt.Println(c.Get(4))    // returns 4
 	fmt.Println(c.Delete(3)) // returns 3
 	fmt.Println(c)
-	fmt.Println(c.Get(3)) // returns -1 (not found)
+	fmt.Println(c.Get(3))     // returns -1 (not found)
+	fmt.Println(c.Delete(99)) // returns -1 (not found)
+	fmt.Println(c.Get(100))   // returns -1 (not found)
 
 	/*
 	   Output of testing the above on debugging session vscode
@@ -83,5 +101,29 @@ func main() {
 	   3
 	   {2 map[4:4] [3 4]}
 	   -1
+	   -1
+	   -1
+	*/
+
+	/*
+		Should fail tests
+
+		c2 := Constructor(0) // Capacity is 0. Should panic.
+		c2.Put(1, 1)
+
+		c2 := Constructor(1001) // Capacity is 1001. Should panic.
+		c2.Put(1, 1)
+
+		c2 := Constructor(2)
+		c2.Put(-1, 1) // Key is negative. Should panic.
+
+		c2 := Constructor(2)
+		c2.Put(104, 1) // Key is greater than 103. Should panic.
+
+		c2 := Constructor(2)
+		c2.Put(1, -1) // Value is negative. Should panic.
+
+		c2 := Constructor(2)
+		c2.Put(1, 106) // // Value is greater than 105. Should panic.
 	*/
 }
