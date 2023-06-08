d2faec9e52e6e66c1b74aaaf356a448acbb324e9
Bruno Hidalgo <b.h.lemergas@gmail.com>
2023-06-08 19:48:02 +1200

    add: cache struct and constructor

--- lru.go
+++ lru.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+type LRUCache struct {
+	capacity int
+	cache    map[int]int
+	keys     []int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]int),
+		keys:     make([]int, 0, capacity),
+	}
+}
+
+func main() {
+	c := Constructor(2)
+	fmt.Println(c)
+	fmt.Println("It works")
+}
