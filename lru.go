7b4c5d25d081be16daa360b6aa0de3279b38d1a6
Bruno Hidalgo <b.h.lemergas@gmail.com>
2023-06-08 21:03:57 +1200

    fix: var name

--- lru.go
+++ lru.go
@@ -27,9 +27,9 @@ func remove(slice []int, s int) []int {
 
 func (c *LRUCache) Put(key int, value int) {
 	if len(c.keys) >= c.capacity {
-		oldkey := c.keys[0]
+		oldKey := c.keys[0]
 		c.keys = c.keys[1:]
-		delete(c.cache, oldkey)
+		delete(c.cache, oldKey)
 	}
 	c.cache[key] = value
 	c.keys = append(c.keys, key)
@@ -53,26 +53,9 @@ func (c *LRUCache) Delete(key int) int {
 }
 
 func main() {
-	c := Constructor(2)
-	/*
-		c.Put(1, 1)
-		fmt.Println(c)
-		fmt.Println(c.Get(1))
-		c.Delete(1)
-		c.Put(2, 2)
-		fmt.Println(c)
-		c.Put(3, 3)
-		fmt.Println(c.Get(2))
-		fmt.Println(c.Get(2))
-		fmt.Println(c)
-		c.Put(4, 4)
-		fmt.Println(c.Get(2))
-		fmt.Println(c.Get(2))
-		fmt.Println(c)
-		fmt.Println("Exiting")
-	*/
-	// Testing
 
+	// Testing
+	c := Constructor(2)
 	c.Put(1, 1)
 	c.Put(2, 2)
 	fmt.Println(c.Get(1)) // returns 1
