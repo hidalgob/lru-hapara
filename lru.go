47aa543256e1fce6d4c0bde83a2f2f2c8a2fb091
Bruno Hidalgo <b.h.lemergas@gmail.com>
2023-06-08 20:19:12 +1200

    add: keys put and get

--- lru.go
+++ lru.go
@@ -18,10 +18,12 @@ func Constructor(capacity int) LRUCache {
 
 func (this *LRUCache) Put(key int, value int) {
 	this.cache[key] = value
+	this.keys = append(this.keys, key)
 }
 
 func (this *LRUCache) Get(key int) int {
 	if val, ok := this.cache[key]; ok {
+		this.keys = append(this.keys, key)
 		return val
 	}
 	return -1
